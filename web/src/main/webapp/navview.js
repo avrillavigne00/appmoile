b3e0013313d3a1cc0dfc8be9619af5a9d02e2474
avrillavigne0 <avril.lavigne.cn@gmail.com>
2016-06-11 23:20:39 +0800

    init6

---
+++ web/src/main/webapp/navview.js
@@ -0,0 +1,31 @@
+
+define(["zepto",
+        "underscore",
+        "backbone",       
+        "text!../../../nav.tpl.html"],
+	function($,_,Backbone,template){
+		var navView = Backbone.View.extend({
+			el:$("#footer"),
+			template:_.template(template),			
+			render:function(){
+				this.$el.html(this.template({}));				
+				return this;
+			},
+			initialize:function(option){
+				
+				this.render();
+			},
+			setItemActive:function(index){
+				this.$el.find(".tab-item").removeClass("active");
+				this.$el.find(".tab-item").eq(index).addClass("active");  
+			},
+			hide:function(){
+                this.$el.hide();            
+			},
+			show:function(){
+                this.$el.show();              
+			}			
+		});
+		return navView;
+	}
+);
